2177735d691ac9068c2d948d7cbd2c853ccaf936
hseong3243 <hseong3243@gmail.com>
2024-04-15 17:23:43 +0900

    feat: 메인 탭 페이지 추가

---
+++ src/components/Tabs.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  name: "Tabs",
+  data() {
+    return {
+      tab: 1,
+    }
+  }
+}
+</script>
+
+<template>
+  <v-card>
+    <v-tabs
+        v-model="tab">
+      <v-tab value="1">링크 추가</v-tab>
+      <v-tab value="2">허브 링크 추가</v-tab>
+      <v-tab value="3">로그인</v-tab>
+    </v-tabs>
+    <v-card-text>
+      <v-window v-model="tab">
+        <v-window-item value="1">
+        </v-window-item>
+        <v-window-item value="2">
+        </v-window-item>
+        <v-window-item value="3">
+        </v-window-item>
+      </v-window>
+    </v-card-text>
+  </v-card>
+</template>
+
+<style scoped>
+
+</style>
