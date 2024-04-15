af5f7197942365576eada2d1431651bfd882de1b
hseong3243 <hseong3243@gmail.com>
2024-04-15 17:31:32 +0900

    feat: 로그인 페이지 추가

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -1,9 +1,22 @@
 <script>
+import LoginPage from "@/components/login/LoginPage.vue";
+import {useAuthStore} from "@/store/AuthStore.js";
+
 export default {
   name: "Tabs",
+  components: {LoginPage},
+  setup() {
+    const authStore = useAuthStore();
+    return {authStore}
+  },
   data() {
     return {
       tab: 1,
+    }
+  },
+  methods: {
+    changeTabNumber() {
+      this.tab = 1;
     }
   }
 }
@@ -11,11 +24,10 @@ export default {
 
 <template>
   <v-card>
-    <v-tabs
-        v-model="tab">
+    <v-tabs v-model="tab">
       <v-tab value="1">링크 추가</v-tab>
       <v-tab value="2">허브 링크 추가</v-tab>
-      <v-tab value="3">로그인</v-tab>
+      <v-tab value="3" v-if="!authStore.isLogin">로그인</v-tab>
     </v-tabs>
     <v-card-text>
       <v-window v-model="tab">
@@ -24,6 +36,7 @@ export default {
         <v-window-item value="2">
         </v-window-item>
         <v-window-item value="3">
+          <LoginPage @loginEvent="changeTabNumber"/>
         </v-window-item>
       </v-window>
     </v-card-text>
