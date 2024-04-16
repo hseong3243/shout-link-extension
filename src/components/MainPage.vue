41fd73f376b4fd28efcda2b1d3e60fa62af734da
hseong3243 <hseong3243@gmail.com>
2024-04-16 10:08:42 +0900

    feat: 로그인 페이지와 탭 분리

---
+++ src/components/MainPage.vue
@@ -0,0 +1,25 @@
+<script>
+import Tabs from "@/components/Tabs.vue";
+import {useAuthStore} from "@/store/AuthStore.js";
+import LoginPage from "@/components/login/LoginPage.vue";
+
+export default {
+  name: "MainPage",
+  components: {LoginPage, Tabs},
+  setup() {
+    const authStore = useAuthStore();
+    return {authStore}
+  }
+}
+</script>
+
+<template>
+  <div>
+    <Tabs v-if="authStore.isLogin"/>
+    <LoginPage v-if="!authStore.isLogin"/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
