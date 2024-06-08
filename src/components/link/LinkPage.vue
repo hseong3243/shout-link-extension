4bd1e1259fd55f543dc271643e35056d8f75add8
hseong3243 <hseong3243@gmail.com>
2024-06-08 20:14:17 +0900

    feat: 링크 추가 시 만료일 지정 추가

--- src/components/link/LinkPage.vue
+++ src/components/link/LinkPage.vue
@@ -1,18 +1,24 @@
 <script>
 import api from "@/axios/index.js";
+import VueDatePicker from '@vuepic/vue-datepicker'
+import '@vuepic/vue-datepicker/dist/main.css'
+import dayjs from "dayjs";
 
 export default {
   name: "LinkPage",
+  components: {VueDatePicker},
   data() {
     return {
       selectedLinkBundle: {},
       linkBundles: [
         {}
       ],
+      selectedExpiredAt: false,
       createLinkRequest: {
         linkBundleId: Number,
         url: "",
         description: "",
+        expiredAt: null,
       },
       dataReady: false,
     }
@@ -34,15 +40,20 @@ export default {
       const data = response.data;
       this.linkBundles = data.linkBundles;
       const defaultLinkBundle = this.linkBundles.filter(linkBundle => linkBundle.isDefault)
-      if(defaultLinkBundle.length !== 0) {
+      if (defaultLinkBundle.length !== 0) {
         this.selectedLinkBundle = defaultLinkBundle[0];
       }
     },
     createLinkApiCall() {
+      let expiredAtStr = '';
+      if (this.createLinkRequest.expiredAt !== null) {
+        expiredAtStr = dayjs(this.createLinkRequest.expiredAt).format('YYYY-MM-DDTHH:mm:ss');
+      }
       api.post('/api/links', {
         linkBundleId: this.selectedLinkBundle.linkBundleId,
         url: this.createLinkRequest.url,
-        description: this.createLinkRequest.description
+        description: this.createLinkRequest.description,
+        expiredAt: expiredAtStr,
       })
       .then((response) => {
         alert('새로운 링크가 추가되었습니다!');
@@ -75,6 +86,20 @@ export default {
         density="compact"
         v-model="createLinkRequest.description"
     ></v-text-field>
+    <div class="d-flex ga-1">
+      <input type="checkbox" v-model="selectedExpiredAt"/>
+      <p>만료일 지정</p>
+    </div>
+    <div class="d-flex justify-center pa-1" v-if="selectedExpiredAt">
+      <vue-date-picker
+          v-model="createLinkRequest.expiredAt"
+          class="w-75"
+          locale="ko"
+          timezone="Asia/Seoul"
+          inline
+          auto-apply>
+      </vue-date-picker>
+    </div>
     <div class="text-end">
       <v-btn
           variant="outlined"
